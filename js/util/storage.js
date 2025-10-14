d506bb627c09655b8fd9fc636aca323669259eef
Stan <stanthemanfaceman@gmail.com>
2025-10-13 21:49:36 -0400

    Add files via upload

---
+++ js/util/storage.js
@@ -0,0 +1,17 @@
+const KEYS = {
+  HAS_OPENED_SAVE_SLOT: 'hasOpenedSaveSlot'
+};
+
+export function getHasOpenedSaveSlot() {
+  return localStorage.getItem(KEYS.HAS_OPENED_SAVE_SLOT) === 'true';
+}
+
+export function setHasOpenedSaveSlot(value) {
+  localStorage.setItem(KEYS.HAS_OPENED_SAVE_SLOT, value ? 'true' : 'false');
+}
+
+export function ensureStorageDefaults() {
+  if (localStorage.getItem(KEYS.HAS_OPENED_SAVE_SLOT) === null) {
+    setHasOpenedSaveSlot(false);
+  }
+}
