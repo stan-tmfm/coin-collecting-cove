801d17d5dbc21ff4678f766589e90b2c3fe36da5
Stan <stanthemanfaceman@gmail.com>
2025-10-20 15:00:34 -0400

    Update shopOverlay.js

--- js/ui/shopOverlay.js
+++ js/ui/shopOverlay.js
@@ -16,7 +16,7 @@ const TRANSPARENT_PX =
   'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mP8/x8AAwMCAO3x0S8AAAAASUVORK5CYII=';
 
 // Dynamic count of upgrades (editable later via setUpgradeCount)
-let UPGRADE_COUNT = 30;
+let UPGRADE_COUNT = 26;
 
 // Upgrades registry (minimal for now)
 let upgrades = {};
@@ -428,6 +428,7 @@ export function setUpgradeCount(n) {
   renderShopGrid();
 }
 export function getUpgrades() { return upgrades; }
+
 
 
 
